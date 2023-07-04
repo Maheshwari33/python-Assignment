809636678da59a04d6c1aaffd64afa9d32177781
Maheshwari33 <137779729+Maheshwari33@users.noreply.github.com>
2023-07-04 22:28:16 +0530

    Update Assi_9.py

--- Assi_9.py
+++ Assi_9.py
@@ -51,3 +51,34 @@ else:
 # print("Multiplication:", mul)
 # print("Division:", div)
 # print("Square Root:", sqr)
+
+
+#mathopr file
+import math 
+class MathOperations:
+    def _init_(self,num1=0,num2=0):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return(self.num1 + self.num2)
+        
+    def subtract(self):
+        return(self.num1 - self.num2)
+        
+    def multiply(self):
+        return(self.num1 * self.num2)
+        
+    def divide(self):
+        if self.num2 != 0:
+            return (self.num1 // self.num2)
+        else:
+            raise ZeroDivisionError("Cannot divide by zero!")
+    def square_root(self):
+        if self.num1 >= 0:
+            return( math.sqrt(self.num1))
+        else:
+            raise ValueError("Cannot calculate square root of a negative number!")
+    
+    def sincalculate(self):
+        return math.sin(self.num1) , math.sin(self.num2)
